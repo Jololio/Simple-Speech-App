3696f46d7466b68cf9c062afd5225a8b662d9dca
Jololio <jala9132@740s-pl-120.local>
2016-09-06 13:37:02 -0600

    Added Text
    Made the app speak and made a few classes to make it to that.

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,8 @@
+package stupid.controller;
+
+public class StupidController {
+	public void start()
+	{
+		System.out.println("This text is dumb :D");
+	}
+}
--- src/stupid/controller/StupidRunner.java
+++ src/stupid/controller/StupidRunner.java
@@ -3,6 +3,8 @@ package stupid.controller;
 public class StupidRunner {
 	public static void main (String [] args)
 	{
-		
+		StupidController stupidAppController = new StupidController();
+		stupidAppController.start();
 	}
 }
+
