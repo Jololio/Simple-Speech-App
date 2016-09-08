1b8b857e55d78475ce8e42fc981c098f60af4b0a
Jololio <jala9132@740s-pl-120.local>
2016-09-08 12:51:58 -0600

    Redesigned app
    Made the app speak to user, and allowed user to respond to the app
    allowing the app to respond.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,8 +1,30 @@
 package stupid.controller;
 
-public class StupidController {
+import java.util.Scanner;
+
+public class StupidController 
+{
+	private String myName;
+	private Scanner readKeyboardInput; 
+	
+	public StupidController()
+	{
+		myName = "Hey, my name is Joseph!";
+		readKeyboardInput = new Scanner(System.in);
+	}
+	
 	public void start()
 	{
-		System.out.println("This text is dumb :D");
+		System.out.println("Nice to meet you!");
+		System.out.println(myName);
+		askQuestions();
 	}
+
+	private void askQuestions()
+	{
+		System.out.println("What is your name?");
+		String userInput = readKeyboardInput.nextLine();
+		System.out.println("Hello there, " + userInput + "!");
+	}
+
 }
