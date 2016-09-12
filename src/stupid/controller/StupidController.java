0fe63983956f61726a5f1ce411127059340aec4b
Jololio <jala9132@740s-pl-120.local>
2016-09-12 12:27:48 -0600

    Added Questions

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -25,6 +25,34 @@ public class StupidController
 		System.out.println("What is your name?");
 		String userInput = readKeyboardInput.next();
 		System.out.println("Hello there, " + userInput + "!");
+		
+		System.out.println("How old are you?");
+		int otherInput = readKeyboardInput.nextInt();
+		System.out.println("Wow, you're " + otherInput + "? That's cool!");
+		
+		System.out.println("What grade are you in?");
+		double moreInput = readKeyboardInput.nextDouble();
+		System.out.println("Wow, I didn't kndow you were in the " + moreInput + " grade.");
+		
+		System.out.println("What kind of Pet do you have?");
+		String sickInput = readKeyboardInput.nextLine();
+		System.out.println("It must be sick to own a " + sickInput + ".");
+		
+		System.out.println("What's your favorite color?");
+		String greatInput = readKeyboardInput.nextLine();
+		System.out.println(greatInput + " is my favorite too!");
+		
+		System.out.println("What is your Mom's name?");
+		String momInput = readKeyboardInput.nextLine();
+		System.out.println(momInput + " is a pretty name.");
+		
+		System.out.println("How many siblings do you have?");
+		int siblingInput = readKeyboardInput.nextInt();
+		System.out.println("You have " + siblingInput + "? I have none.");
+		
+		System.out.println("What's your favorite activity?");
+		String activityInput = readKeyboardInput.nextLine();
+		System.out.println(activityInput + " sounds pretty fun actually.");
 	}
 
 }
